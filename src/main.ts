42b25a6f3afba67b8ae1d2bf528b74578f3ca82f
Rafia Iftikhar <itxrafia190@gmail.com>
2026-04-23 01:40:16 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src=${viteLogo} class="vite" alt="Vite logo" />
-  </div>
+document.querySelector('#app')!.innerHTML = `
   <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo vite" alt="Vite logo" />
+    </a>
+    <h1>My Ecommerce Project</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
   </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+`
+
+setupCounter(document.querySelector('#counter')!)
